2ba01eba81f61ca609b2d0edfb0061650e70da85
Rafael <rafael12silva73@gmail.com>
2026-06-05 14:36:07 -0300

    adicionado client de email

---
+++ src/main/java/com/javanauta/bffagendadortarefas/infrastructure/client/EmailClient.java
@@ -0,0 +1,17 @@
+package com.javanauta.bffagendadortarefas.infrastructure.client;
+
+import com.javanauta.bffagendadortarefas.business.dto.in.TarefasDTORequest;
+import com.javanauta.bffagendadortarefas.business.dto.out.TarefasDTOResponse;
+import com.javanauta.bffagendadortarefas.business.enums.StatusNotificacaoEnum;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@FeignClient(name = "notificacao", url = "${notificacao.url}")
+public interface EmailClient {
+    void enviarEmail(@RequestBody TarefasDTOResponse dto);
+}
